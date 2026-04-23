535046014ff6a314f794b69cca19bf94676167f4
Sai Chepuri <sai.chepuri6@gmail.com>
2026-04-23 16:10:21 -0400

    Installed necessary dependencies.

---
+++ test.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{"role": "user", "content": "Say hello"}]
+)
+
+print(response.choices[0].message.content)
